bd82c712056c39172a83af79c73b30762da85af3
PH_Delporte <50957419+PhDelporte@users.noreply.github.com>
2019-05-31 11:53:55 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html>
 <html> 
-    <h1>je suis une page php</h1>
-	<p>Bienvenue sur le site Cesi@Store, le site officiel du magasin CesiStore ! Vous pourrez trouver ici les meilleurs DVD et livres du moments a des prix jamais vu !</p>
+	<h1>je suis une page php</h1>
+		<p>Bienvenue sur le site Cesi@Store, le site officiel du magasin CesiStore ! Vous pourrez trouver ici les meilleurs DVD et livres du moments a des prix jamais vu !</p>
+	<h2>Nos catégories</h2>
+		<ol>
+			<li>CD</li>
+			<li>DVD</li>
+			<li>Livres</li>
+		</ol>
 </html
