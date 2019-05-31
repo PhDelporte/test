8e7fe4844a6419c4aa377a3838329c21c9601d91
PH <delportepolo@hotmail.fr>
2019-05-31 11:50:34 +0200

    Ajout paragraphe de présentation

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
 <html> 
     <h1>je suis une page php</h1>
-</html>
+	<p>Bienvenue sur le site Cesi@Store, le site officiel du magasin CesiStore ! Vous pourrez trouver ici les meilleurs DVD et livres du moments a des prix jamais vu !</p>
+</html
